e3bcd9f6ec9cd3c9ac308966917302aa3ed50262
TriSetianto123 <165136500+TriSetianto123@users.noreply.github.com>
2024-03-27 14:31:12 +0700

    Add files via upload

---
+++ tugasjs/main.js
@@ -0,0 +1,42 @@
+document.getElementById("simpan").addEventListener(
+    "click",
+    function () {
+      let nama = document.getElementById("nama").value;
+      let npm = document.getElementById("npm").value;
+      let mhs = { nama: nama, npm: npm };
+      let listmhs = [];
+      if (localStorage.getItem("mhs")) {
+        listmhs = JSON.parse(localStorage.getItem("mhs"));
+        listmhs.push(mhs);
+      } else {
+        listmhs.push(mhs);
+      }
+      localStorage.setItem("mhs", JSON.stringify(listmhs));
+      let data = "";
+      for (const [idx, dt] of listmhs.entries()) {
+        data +=
+          "<tr>" +
+          "<th scope='row'>" +
+          (idx + 1) +
+          "</th>" +
+          "<td>" +
+          dt.npm +
+          "</td>" +
+          "<td>" +
+          dt.nama +
+          "</td>" +
+          
+          "</tr>";
+      }
+      document.getElementById("data").innerHTML = data;
+    },
+    true
+  );
+
+
+  document.getElementById("hapus").addEventListener("click", function() {
+        localStorage.clear();
+        document.getElementById("data").innerHTML = "";
+    });
+    
+    
